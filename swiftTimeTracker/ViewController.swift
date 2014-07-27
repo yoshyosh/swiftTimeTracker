7245b6ebefde06b3075aa06d40ec0fa9e47ae59f
yoshyosh <janderson@gmail.com>
2014-07-26 20:37:34 -0700

    Added basic timer functionality, still need logic to convert seconds to minutes and hours

--- swiftTimeTracker/ViewController.swift
+++ swiftTimeTracker/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var currentTimerLabel: UILabel!
+    var appTimer = NSTimer()
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func startTimerButtonDidPress(sender: AnyObject) {
+        // Need general explainations on the different methods
+        self.appTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "incrementCurrentTimeLabel", userInfo: nil, repeats: true)
+    }
+    
+    func incrementCurrentTimeLabel(){
+        var currentTime = (self.currentTimerLabel.text as NSString).intValue
+        var updatedTime = ++currentTime
+        self.currentTimerLabel.text = NSString(format: "%d", updatedTime)
+        
+        //Stops timer after 11 seconds
+        if (updatedTime > 10){
+            self.appTimer.invalidate()
+        }
+    }
 
 }
 
